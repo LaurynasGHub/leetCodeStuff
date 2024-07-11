7f4b63532e284f77829ec5e6c2f40cef2f2b778f
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-07-11 19:31:20 +0300

    push 2 questions from leetcode

---
+++ greatestCommonDivisorOfStrings.js
@@ -0,0 +1,20 @@
+var str1 = 'AABCABDCABCA';
+var str2 = 'ABAB';
+
+function gcdOfStrings(str1, str2) {
+  for (i = 0; i < str1.length; i++) {
+    let slicedPart = str1.slice(i, i + str2.length);
+
+    if (slicedPart === str2) {
+      for (i = 0; i < str2.length; i++) {
+        let slicedPart2 = str2.slice(i, i);
+      }
+
+      return slicedPart;
+    }
+  }
+
+  return '';
+}
+
+gcdOfStrings(str1, str2);
---
+++ mergeStringsAlternatively.js
@@ -0,0 +1,20 @@
+var word1 = 'chair';
+var word2 = 'kebabas';
+
+function mergeAlternatively(word1, word2) {
+  let mergedString = '';
+
+  for (let i = 0; i < word2.length; i++) {
+    if (typeof word1[i] !== 'undefined') {
+      mergedString += word1[i];
+    }
+
+    mergedString += word2[i];
+  }
+
+  console.log(mergedString);
+
+  return mergeAlternatively;
+}
+
+const answer = mergeAlternatively(word1, word2);
