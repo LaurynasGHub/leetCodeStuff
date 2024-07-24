c1ad99951341e35a8c884ab40f91cb223ef76097
Laurynas Gailius <laurynasgailius1@gmail.com>
2024-07-24 15:21:01 +0300

    Create jumpGame1.py

---
+++ jumpGame1.py
@@ -0,0 +1,15 @@
+class Solution:
+    #
+    # QUESTION no. 55
+    # METHODS- Greedy
+    #
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            print('i-', i, 'nums[i]-',nums[i])
+            if i + nums[i] >= goal:
+                goal = i
+                print('goal-',goal)
+        
+        return True if goal <= 0 else False
