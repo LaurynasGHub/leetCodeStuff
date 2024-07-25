db44201007144b2b3acec8ab62a501fdf890996c
Laurynas Gailius <laurynasgailius1@gmail.com>
2024-07-25 11:10:58 +0300

    Create containsDuplicate.js

---
+++ containsDuplicate.js
@@ -0,0 +1,15 @@
+var containsNearbyDuplicate = function(nums, k) {
+    //
+    // QUESTION no- 219
+    // METHODS- Sliding Window, Hash Table, Map
+    //
+    const hasmap = new Map();
+    for (let idx = 0; idx < nums.length; idx++) {
+        // Check if the difference betweend duplicates is less than k
+        if (idx - hasmap.get(nums[idx]) <= k) {
+            return true;
+        }
+        hasmap.set(nums[idx], idx);
+    }
+    return false;
+};
